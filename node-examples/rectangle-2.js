15d5318831d165ff219fa28fa7a0ba8702e3e1b5
samaelxz <samaelxz@163.com>
2017-08-10 18:13:25 +0800

    mofify rectangle-2

--- node-examples/rectangle-2.js
+++ node-examples/rectangle-2.js
@@ -1,8 +1,16 @@
 
-module.exports = function(x,y,callback) {
+module.exports = (x,y,callback) => {
 	try {
 		if (x < 0 || y < 0) {
-			throw new Error("Rectangle dimensions should be greater than zero: l = " + a +)
+			throw new Error("Rectangle dimensions should be greater than zero: l = " + x + ", and b = " + y);			
 		}
+		else
+			callback(null, {
+				perimeter: () => 2*(x+y),
+				area: () => x*y 
+			});
+	}
+	catch (error) {
+		callback(error, null);
 	}
 }
